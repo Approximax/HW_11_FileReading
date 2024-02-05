63abfc6c9d80e98ea3fe0a3124ce30e17f410683
Vladislav Kryukov <vlad_pw_v@list.ru>
2024-02-05 19:19:26 +0300

    Initial commit

---
+++ src/test/java/PDFReadFromZipTest.java
@@ -0,0 +1,26 @@
+import com.codeborne.pdftest.PDF;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class PDFReadFromZipTest {
+
+    private final ClassLoader classLoader = PDFReadFromZipTest.class.getClassLoader();
+
+    @Test
+    void pdfParsingTest() throws Exception {
+        try (InputStream inputStream = classLoader.getResourceAsStream("test-resources.zip");
+             ZipInputStream zipInputStream = new ZipInputStream(inputStream)) {
+            ZipEntry entry;
+            while ((entry = zipInputStream.getNextEntry()) != null) {
+                if (entry.getName().endsWith(".pdf")) {
+                    PDF pdf = new PDF(zipInputStream);
+                    Assertions.assertEquals("PDF INFO & TEST FILE", pdf.title);
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/XLSReaderFromZipTest.java
@@ -0,0 +1,31 @@
+import com.codeborne.xlstest.XLS;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class XLSReaderFromZipTest {
+
+    private final ClassLoader classLoader = XLSReaderFromZipTest.class.getClassLoader();
+
+    @Test
+    void xlsParsingTest() throws Exception {
+        try (InputStream inputStream = classLoader.getResourceAsStream("test-resources.zip");
+             ZipInputStream zipInputStream = new ZipInputStream(inputStream)) {
+            ZipEntry entry;
+            while ((entry = zipInputStream.getNextEntry()) != null) {
+                if (entry.getName().endsWith(".xlsx")) {
+                    XLS xls = new XLS(zipInputStream);
+                    Assertions.assertEquals("Уборщик",
+                            xls.excel.getSheet("Sheet1")
+                                    .getRow(4)
+                                    .getCell(2)
+                                    .getStringCellValue()
+                            );
+                }
+            }
+            }
+        }
+    }
